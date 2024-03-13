d3bbd10f14d2192e0bec7e07e57d636d9d293049
liu.yuqiang <liuyuqiang@sihesci-tech.com>
2024-03-13 11:11:35 +0800

    init lambda

--- lib/metric-alarm-stack.ts
+++ lib/metric-alarm-stack.ts
@@ -1,16 +1,25 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as events from 'aws-cdk-lib/aws-events';
+import * as targets from 'aws-cdk-lib/aws-events-targets';
 
 export class MetricAlarmStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // 创建 Python Lambda 函数
+    const pythonLambda = new lambda.Function(this, 'MyPythonLambda', {
+      runtime: lambda.Runtime.PYTHON_3_8,
+      handler: 'index.handler', // 指定 Lambda 处理程序的入口函数
+      code: lambda.Code.fromAsset('path/to/python-lambda-code'), // 替换为您的 Python Lambda 代码路径
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'MetricAlarmQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // 创建定时触发器，每天执行一次
+    const rule = new events.Rule(this, 'MyRule', {
+      schedule: events.Schedule.cron({ minute: '0', hour: '1' }), // 每天凌晨 1 点执行
+    });
+    rule.addTarget(new targets.LambdaFunction(pythonLambda));
+  }
   }
 }
