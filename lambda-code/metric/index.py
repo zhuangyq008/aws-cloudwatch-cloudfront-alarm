8a034384d2a33669402bdaed79a50127aef2671b
liu.yuqiang <liuyuqiang@sihesci-tech.com>
2024-03-13 17:03:57 +0800

    add call subaccounts_and_metrics_alarm function

--- lambda-code/metric/index.py
+++ lambda-code/metric/index.py
@@ -1,8 +1,19 @@
 # index.py
+from metric import subaccounts_and_metrics_alarm
 
 def handler(event, context):
-    # 在这里编写您的 Python 代码逻辑
-    print("Hello from Python Lambda!")
-    # 可以执行其他操作，例如调用其他 AWS 服务、处理数据等
-
-    return "Lambda execution completed."
+    account_configs = [
+        {
+            'account_id': '611234940057',
+            'assume_role_arn': 'arn:aws:iam::611234940057:role/OrganizationAccountAccessRole',
+            'period': 300,
+            'minutes': 180,
+            'threshold': 1,
+            'consecutive_points': 1,
+            'payer_topic_name' : 'metric-alarm-topic'
+            # ,'payer_email_addresses' : ['jarrywen@163.com', 'jarrywenjack@gmail.com']
+        },
+        # Add more account configurations here if needed
+    ]
+    metrics_data = subaccounts_and_metrics_alarm(account_configs)
+    print(metrics_data)
